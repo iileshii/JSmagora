3c269b976d36a1f26fa80294422a94e755d7542d
Alexey Kuznetsov <kuznetsov.a.e@gmail.com>
2015-06-19 17:14:33 +0600

    I get array, but did not see it

--- app/app.js
+++ app/app.js
@@ -12,19 +12,23 @@ angular.module('mgcrea.ngStrapDocs')
 
       $scope.selectedAddress = '';
       $scope.getAddress = function(viewValue) {
-        var params = {address: viewValue, sensor: false};
-        return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
-            .then(function(res) {
-              return res.data.results;
-            });
+          var params = {address: viewValue, sensor: false};
+          return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+              .then(function (res) {
+                  return res.data.results;
+              });
       };
+
 
       $scope.selectedAddress2 = '';
       $scope.getAddress2 = function(viewValue) {
-        var params = {address: viewValue, sensor: false};
-        return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+          var params = {property: 'own', limit: '1500', city: '54'};
+        return $http.get('http://json.alfabank.ru/atm/0.1/list', {params: params})
             .then(function(res) {
-              return res.data.results;
+              var objArr = res.data.response.data;
+                var myArr = Object.keys(objArr).map(function(k) { return objArr[k] });
+                var newArray = myArr.filter(function (el) {   return el.lat > 55.1});
+                return myArr;
             });
       };
 
