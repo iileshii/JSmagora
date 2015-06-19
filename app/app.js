c6b794207c39c8309b708e66b69c7ec674737c3e
Alexey Kuznetsov <kuznetsov.a.e@gmail.com>
2015-06-19 15:57:38 +0600

    Only address field

--- app/app.js
+++ app/app.js
@@ -1,12 +1,33 @@
+var app = angular.module('mgcrea.ngStrapDocs', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
+
+app.controller('MainCtrl', function($scope) {
+});
+
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('mgcrea.ngStrapDocs')
+
+    .controller('TypeaheadDemoCtrl', function($scope, $templateCache, $http) {
+
+      $scope.selectedState = '';
+      $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+      $scope.selectedIcon = '';
+      $scope.icons = [
+        {value: 'Gear', label: '<i class="fa fa-gear"></i> Gear'},
+        {value: 'Globe', label: '<i class="fa fa-globe"></i> Globe'},
+        {value: 'Heart', label: '<i class="fa fa-heart"></i> Heart'},
+        {value: 'Camera', label: '<i class="fa fa-camera"></i> Camera'}
+      ];
+
+      $scope.selectedAddress = '';
+      $scope.getAddress = function(viewValue) {
+        var params = {address: viewValue, sensor: false};
+        return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+            .then(function(res) {
+              return res.data.results;
+            });
+      };
+
+    });
+
