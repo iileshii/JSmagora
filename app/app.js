11e62a827eb0735d283adf07cab45064d660ecd5
Alexey Kuznetsov <kuznetsov.a.e@gmail.com>
2015-06-21 15:23:34 +0600

    It can find what you want!

--- app/app.js
+++ app/app.js
@@ -10,25 +10,24 @@ angular.module('mgcrea.ngStrapDocs')
     .controller('TypeaheadDemoCtrl', function($scope, $templateCache, $http) {
 
 
-      $scope.selectedAddress = '';
-      $scope.getAddress = function(viewValue) {
-          var params = {address: viewValue, sensor: false};
-          return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
-              .then(function (res) {
-                  return res.data.results;
-              });
-      };
 
-
-      $scope.selectedAddress2 = '';
-      $scope.getAddress2 = function(viewValue) {
+      $scope.selectedATM = '';
+      $scope.getATM = function(viewValue) {
           var params = {property: 'own', limit: '1500', city: '54'};
         return $http.get('http://json.alfabank.ru/atm/0.1/list', {params: params})
             .then(function(res) {
               var objArr = res.data.response.data;
-                var myArr = Object.keys(objArr).map(function(k) { return objArr[k] });
-                var newArray = myArr.filter(function (el) {   return el.lat > 55.1});
-                return myArr;
+                var myArr = Object.keys(objArr).map(function(k) {
+                    objArr[k].title = objArr[k].title.replace("&laquo;", "\"");
+                    objArr[k].title = objArr[k].title.replace("&raquo;", "\"");
+                    objArr[k].title = objArr[k].title.replace("&nbsp;", " ");
+                    objArr[k].address = objArr[k].address.replace("&laquo;", "\"");
+                    objArr[k].address = objArr[k].address.replace("&raquo;", "\"");
+                    objArr[k].address = objArr[k].address.replace("&nbsp;", " ");
+                    return objArr[k];
+                });
+                var newArray = myArr.filter(function (el) {   return el.title.indexOf(viewValue) > -1 || el.address.indexOf(viewValue) > -1 });
+                return newArray;
             });
       };
 
